6eafe377168c69ba212638eaedf031e8be8c2d31
utkanc <utkan@tiramisu.com.tr>
2021-10-13 19:11:32 +0300

    initial commit

---
+++ Assets/EntrenceManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntrenceManager : MonoBehaviour
+{
+    public static EntrenceManager instance;
+    public List<EntrenceBehaviour> AllEntrences = new List<EntrenceBehaviour>();
+
+    private void OnEnable()
+    {
+        instance = this;
+    }
+     
+}
